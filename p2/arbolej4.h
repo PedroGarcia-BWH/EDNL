8c894985f5ed12537f6f51947234826b51b60c3f
Saikora <pedro.garciaromera@alum.uca.es>
2022-03-11 22:22:20 +0100

    subida

--- p2/arbolej4.h
+++ p2/arbolej4.h
@@ -2,18 +2,19 @@
 #define ARBOLEJ4_H
 
 #include <cassert>
+using namespace std;
 
 template <typename T> class Abin{
     public: 
         typedef size_t nodo;
-        static const nodo NODO_NULO
-        explicit Abin(size_t maxNodos, const T& e_nulo = T());
+        static const nodo NODO_NULO;
+        Abin(size_t maxNodos, const T& e_nulo = T());
         void insertarRaiz(const T& elem);
         void insertarHijoIzqdo(nodo n, const T& e);
         void insertarHijoDrcho(nodo n, const T& e);
         nodo hijoIzqdo(nodo n) const ;
         nodo hijoDrcho(nodo n) const ;
-        nodo padre(nodo n) const ;
+        nodo padre(nodo n)  ;
         nodo raiz() const;
         bool arbolVacio() const;
         ~Abin();
@@ -21,15 +22,15 @@ template <typename T> class Abin{
         T& elemento(nodo n);
     private:
         size_t maxNodos;
-        T elto_nulo
+        T elto_nulo;
         T* nodos;
-        int alturaMax
+        int alturaMax(nodo n);
 };
 template <typename T>
     const typename Abin<T>::nodo Abin<T>::NODO_NULO(SIZE_MAX);
 
 template <typename T>
-Abin<T>::Abin(size_t maxNodos, const T& e_nulo): nodos(new T[maxNodos]), elto_nulo(e_nulo), mazNodos(maxNodos){
+Abin<T>::Abin( std::size_t maxNodos, const T& e_nulo): nodos(new T[maxNodos]), elto_nulo(e_nulo), maxNodos(maxNodos){
     for(size_t i =0; i<maxNodos;i++;){
         nodos[i] = elto_nulo;
     }
@@ -42,12 +43,21 @@ void Abin<T>::insertarRaiz(const T& elem){
 }
 
 template<typename T>
-void insertarHijoIzqdo(nodo n, const T& e){
-    nodos[n-pow(2,altura(n)-1)] = e;
+void Abin<T>::insertarHijoIzqdo(nodo n, const T& e){
+    nodos[n-pow(2,alturaMax(n)-1)] = e;
 }
 template <typename T>
-nodo padre(nodo n){
-    if(n < maxNodos/2) return n+ pow(2,altura(n));
-    else return n - pow(2,altura(n));
+Abin<T>::nodo Abin<T>::padre(nodo n){
+    if(n < maxNodos/2) return n+ pow(2,alturaMax(n));
+    else return n - pow(2,alturaMax(n));
+}
+
+template <typename T>
+int Abin<T>::alturaMax(nodo n){
+    if(n == NODO_NULO){
+        return -1;
+    }else{
+
+    }
 }
 #endif
