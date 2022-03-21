62af09da7e22926d3a5af00eedbe4fc2a84ee975
Saikora11 <pedro.garciaromera@alum.uca.es>
2022-03-21 20:26:57 +0100

    terminado tad

--- p2/arbolej4.h
+++ p2/arbolej4.h
@@ -25,16 +25,17 @@ template <typename T> class Abin{
         T& elemento(nodo n);
     private:
         size_t maxAltura;
+        size_t maxNodos;
         T elto_nulo;
         T* nodos;
-        int alturaMax(nodo n);
+        int altura(nodo n);
 };
 template <typename T>
     const typename Abin<T>::nodo Abin<T>::NODO_NULO(SIZE_MAX);
 
 template <typename T>
-Abin<T>::Abin( std::size_t maxAltura, const T& e_nulo): nodos(new T[maxAltura]), elto_nulo(e_nulo), maxAltura(maxAltura){
-    for(size_t i =0; i<maxAltura;i++;){
+Abin<T>::Abin(std::size_t maxAltura, const T& e_nulo): maxNodos(std::pow(2,maxAltura+1)-1),nodos(new T[maxNodos]), elto_nulo(e_nulo), maxAltura(maxAltura){
+    for(size_t i =0; i<maxNodos;i++;){
         nodos[i] = elto_nulo;
     }
 
@@ -42,27 +43,38 @@ Abin<T>::Abin( std::size_t maxAltura, const T& e_nulo): nodos(new T[maxAltura]),
 
 template <typename T>
 void Abin<T>::insertarRaiz(const T& elem){
-    nodos[maxAltura/2]  = elem;
+    nodos[maxNodos/2]  = elem;
 }
 
 template<typename T>
 void Abin<T>::insertarHijoIzqdo(nodo n, const T& e){
-    nodos[n-pow(2,alturaMax(n)-1)] = e;
+    nodos[n-pow(2,altura(n)-1)] = e;
 } 
 
 template <typename T>
 Abin<T>::nodo Abin<T>::padre(nodo n){
     if(n < maxNodos/2) return n+ pow(2,alturaMax(n));
-    else return n - pow(2,alturaMax(n));
+    else return n - pow(2,altura(n));
 }
 
 template <typename T>
-int Abin<T>::alturaMax(nodo n){
-    /*if(n == NODO_NULO){
-        return -1;
-    }else{
-        return std::max(alturamax())
-    }*/
-    if(n > maxAltura)
+int Abin<T>::altura(nodo n){
+    bool terminado = false;
+    int rangoMin = 0;
+    int rangoMax = maxNodos;
+    int altura = 0;
+    while(!terminado) {
+        if(n > rangoMax/2){
+            rangoMin = rangoMax/2;
+
+        }else{
+            rangoMax = rangoMax /2;
+        }
+
+        if(n == rangoMax/2) terminado = true;
+        altura++,
+    }
+
+    return altura;
 }
 #endif
